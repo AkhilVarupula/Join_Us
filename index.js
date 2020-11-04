ec47ed61743bb318632a7bab886cc028b8850c48
Akhil <akhilbpm@gmail.com>
2020-11-04 13:36:16 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var express= require('express');
+var mysql= require('mysql');
+var bodyParser = require("body-parser");
+var app= express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database:"node_project",
+    password:"mymacmysql",
+    insecureAuth : true
+  });
+
+app.get("/", function(req, res){
+    var q = "SELECT COUNT(*) AS count FROM users";
+    connection.query(q, function(err,results){
+        if(err) throw err;
+        var count =results[0].count;
+        res.render("home",{count :count});
+    });
+});
+
+app.post("/register", function(req, res){
+    var email= req.body.email;
+    var person={
+        email:req.body.email
+    }
+    connection.query('INSERT INTO users SET ?', person , function(err, results){
+        if(err) throw err;
+        console.log(results);
+        res.redirect("/");
+    })
+});
+
+app.listen(3000, function(){
+    console.log("Server running !!!");
+});
